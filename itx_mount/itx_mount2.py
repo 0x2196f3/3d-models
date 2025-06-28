34d580f9199ccc164a9055c9c35975c6e5642b4f
0x2196f3 <154529912+0x2196f3@users.noreply.github.com>
2025-06-29 06:16:21 +0800

    Add files via upload

--- itx_mount/itx_mount2.py
+++ itx_mount/itx_mount2.py
@@ -4,7 +4,7 @@ import FreeCAD, Part, FreeCADGui
 doc = FreeCAD.newDocument("CombinedDocument")
 
 # Parameters for the custom shape
-thickness = 3.0  # Thickness of the shapes in mm
+thickness = 2.0  # Thickness of the shapes in mm
 
 # Create the black square (150mm x 150mm) at (0, 200)
 square = Part.makeBox(15, 15, thickness, FreeCAD.Vector(0, 20, 0))
@@ -31,7 +31,7 @@ rotation_angle = 180  # Degrees
 final_shape.rotate(FreeCAD.Vector(0, 0, 0), rotation_axis, rotation_angle)
 
 # Position the final shape at (90mm, 0mm, 3mm)
-final_shape.Placement.Base = FreeCAD.Vector(145, 3, 36)
+final_shape.Placement.Base = FreeCAD.Vector(145, 2, 36)
 
 # Add the final shape to the document
 Part.show(final_shape)
